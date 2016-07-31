90a756b9df73755a526b8e7db24e756260a7cfbe
Julian Pistorius <julian@julianpistorius.com>
2016-07-31 15:43:29 -0700

    Removed generic Entity tests from my specific entity test cases, and ignoring an attribute warning which I test for anyway.

--- tests/test_allocation_source_entity.py
+++ tests/test_allocation_source_entity.py
@@ -1,16 +1,13 @@
 import unittest
 
-import mock
-from eventsourcing.domain.model.entity import mutableproperty, EntityIDConsistencyError, \
-    EntityVersionConsistencyError, EventSourcedEntity, created_mutator, MutatorRequiresTypeError
-from eventsourcing.domain.model.events import DomainEvent, subscribe, unsubscribe, assert_event_handlers_empty
-from eventsourcing.domain.model.exceptions import ProgrammingError
+from atmo_eventsourcing.domain.model.allocation_source import register_new_allocation_source, AllocationSource
+from atmo_eventsourcing.infrastructure.event_sourced_repos.allocation_source_repo import AllocationSourceRepo
+from eventsourcing.domain.model.entity import EntityIDConsistencyError, \
+    EntityVersionConsistencyError
+from eventsourcing.domain.model.events import DomainEvent, assert_event_handlers_empty
 from eventsourcing.infrastructure.event_store import EventStore
 from eventsourcing.infrastructure.persistence_subscriber import PersistenceSubscriber
 from eventsourcing.infrastructure.stored_events.python_objects_stored_events import PythonObjectsStoredEventRepository
-
-from atmo_eventsourcing.domain.model.allocation_source import register_new_allocation_source, AllocationSource
-from atmo_eventsourcing.infrastructure.event_sourced_repos.allocation_source_repo import AllocationSourceRepo
 
 
 class TestAllocationSourceEntity(unittest.TestCase):
@@ -99,80 +96,3 @@ class TestAllocationSourceEntity(unittest.TestCase):
 
         # Check we get an error when attempting to mutate on the event.
         self.assertRaises(NotImplementedError, AllocationSource.mutate, AllocationSource, UnsupportedEvent('1', '0'))
-
-    def test_mutableproperty(self):
-        # Check we get an error when called with something other than a function.
-        self.assertRaises(ProgrammingError, mutableproperty, 'not a getter')
-        self.assertRaises(ProgrammingError, mutableproperty, 123)
-        self.assertRaises(ProgrammingError, mutableproperty, None)
-
-        # Call the decorator with a function.
-        getter = lambda: None
-        p = mutableproperty(getter)
-
-        # Check we got a property object.
-        self.assertIsInstance(p, property)
-
-        # Check the property object has both setter and getter functions.
-        self.assertTrue(p.fset)
-        self.assertTrue(p.fget)
-
-        # Pretend we decorated an object.
-        o = EventSourcedEntity(entity_id='1', entity_version=1, domain_event_id=1)
-        o.__dict__['_<lambda>'] = 'value1'
-
-        # Call the property's getter function.
-        value = p.fget(o)
-        self.assertEqual(value, 'value1')
-
-        # Call the property's setter function.
-        p.fset(o, 'value2')
-
-        # Check the attribute has changed.
-        value = p.fget(o)
-        self.assertEqual(value, 'value2')
-
-        # Check the property's getter function isn't the getter function we passed in.
-        self.assertNotEqual(p.fget, getter)
-
-        # Define a class that uses the decorator.
-        class Aaa(EventSourcedEntity):
-            """An event sourced entity."""
-
-            def __init__(self, a, *args, **kwargs):
-                super(Aaa, self).__init__(*args, **kwargs)
-                self._a = a
-
-            @mutableproperty
-            def a(self):
-                """A mutable event sourced property."""
-
-        # Instantiate the class and check assigning to the property publishes an event and updates the object state.
-        published_events = []
-        subscription = (lambda x: True, lambda x: published_events.append(x))
-        subscribe(*subscription)
-        entity_id = '1'
-        try:
-            aaa = Aaa(entity_id=entity_id, entity_version=0, domain_event_id='0', a=1)
-            self.assertEqual(aaa.a, 1)
-            aaa.a = 'value1'
-            self.assertEqual(aaa.a, 'value1')
-        finally:
-            unsubscribe(*subscription)
-
-        # Check an event was published.
-        self.assertEqual(len(published_events), 1)
-
-        # Check the published event was an AttributeChanged event, with the expected attribute values.
-        published_event = published_events[0]
-        self.assertIsInstance(published_event, Aaa.AttributeChanged)
-        self.assertEqual(published_event.name, '_a')
-        self.assertEqual(published_event.value, 'value1')
-        self.assertTrue(published_event.domain_event_id)
-        self.assertEqual(published_event.entity_id, entity_id)
-
-    def test_static_mutator_method(self):
-        self.assertRaises(NotImplementedError, EventSourcedEntity._mutator, 1, 2)
-
-    def test_created_mutator_error(self):
-        self.assertRaises(MutatorRequiresTypeError, created_mutator, mock.Mock(spec=DomainEvent), 'not a class')
