92a7ee7fb45d541190ac3cc7bc904286d2ea35ff
Julian Pistorius <julian@julianpistorius.com>
2016-08-01 21:38:26 -0700

    Now loading allocation source change data as well as instance status history data.

--- atmo_cqrs/db.py
+++ atmo_cqrs/db.py
@@ -1,15 +1,18 @@
 QUERY_SELECT_EVENTS = 'SELECT * FROM simple_events'
+QUERY_SELECT_EVENTS_BY_EVENT_DATE = 'SELECT * FROM simple_events ORDER BY event_date'
 QUERY_INSERT_EVENT = '''
 INSERT INTO simple_events (
   entity_id,
   event_id,
   timestamp,
+  event_date,
   event_topic,
   event_attrs)
 VALUES (
   :entity_id,
   :event_id,
   :timestamp,
+  :event_date,
   :event_topic,
   :event_attrs
 )'''
@@ -19,6 +22,7 @@ CREATE TABLE simple_events (
   entity_id      TEXT,
   event_id       TEXT,
   timestamp      FLOAT,
+  event_date     FLOAT,
   event_topic    TEXT,
   event_attrs    TEXT
 )'''
--- atmo_cqrs/tests/test_basic.py
+++ atmo_cqrs/tests/test_basic.py
@@ -5,13 +5,17 @@ import json
 import random
 import unittest
 import uuid
+from hashlib import md5
 
 import records
+from dateutil.parser import parse
 from eventsourcing.utils.time import utc_timezone
 from faker import Faker
 
-from atmo_cqrs.db import QUERY_SELECT_EVENTS, QUERY_INSERT_EVENT, QUERY_CREATE_TABLE, QUERY_DROP_TABLE
-from atmo_cqrs.tests.test_data import INSTANCE_STATUSES, STATUS_HISTORY_SIMPLE_TEST_DATA
+from atmo_cqrs.db import QUERY_SELECT_EVENTS, QUERY_INSERT_EVENT, QUERY_CREATE_TABLE, QUERY_DROP_TABLE, \
+    QUERY_SELECT_EVENTS_BY_EVENT_DATE
+from atmo_cqrs.tests.test_data import INSTANCE_STATUSES, INSTANCE_STATUS_HISTORY_DATA_01, \
+    ALLOCATION_SOURCE_CHANGE_DATA_01
 from atmo_eventsourcing.utils.time import datetime_to_timestamp
 
 
@@ -29,7 +33,8 @@ class BasicTestCase(unittest.TestCase):
         super(BasicTestCase, self).tearDown()
 
     def test_random_data(self):
-        for i in xrange(1000):
+        # skip('Skip for now')
+        for i in xrange(10):
             entity_id = i % 20 + 1000
             event_id = str(uuid.uuid4())
             timestamp = datetime_to_timestamp(datetime.datetime.now(tz=utc_timezone))
@@ -41,30 +46,70 @@ class BasicTestCase(unittest.TestCase):
                           entity_id=entity_id,
                           event_id=event_id,
                           timestamp=timestamp,
+                          event_date=timestamp,
                           event_topic=event_topic,
                           event_attrs=event_attrs_json)
         rows = self.db.query(QUERY_SELECT_EVENTS)
         print(rows.export('csv'))
 
-    def test_simple_data(self):
-        status_history_file_object = StringIO.StringIO(STATUS_HISTORY_SIMPLE_TEST_DATA)
-        reader = csv.DictReader(status_history_file_object)
-        for row in reader:
-            entity_id = row['instance_id']
-            event_id = str(row['ih_id'])
-            timestamp = datetime_to_timestamp(datetime.datetime.now(tz=utc_timezone))
-            event_topic = 'atmosphere.model.instance#Instance.StatusChanged'
-            event_attrs = row
-            event_attrs_json = json.dumps(event_attrs)
-            self.db.query(QUERY_INSERT_EVENT,
-                          entity_id=entity_id,
-                          event_id=event_id,
-                          timestamp=timestamp,
-                          event_topic=event_topic,
-                          event_attrs=event_attrs_json)
-
+    def test_instance_status_history(self):
+        load_status_history_data(self.db, INSTANCE_STATUS_HISTORY_DATA_01)
         rows = self.db.query(QUERY_SELECT_EVENTS)
         print(rows.export('csv'))
+
+    def test_allocation_source_change(self):
+        load_allocation_source_change_data(self.db, ALLOCATION_SOURCE_CHANGE_DATA_01)
+        rows = self.db.query(QUERY_SELECT_EVENTS)
+        print(rows.export('csv'))
+
+    def test_instance_status_history_and_allocation_source_change(self):
+        load_status_history_data(self.db, INSTANCE_STATUS_HISTORY_DATA_01)
+        load_allocation_source_change_data(self.db, ALLOCATION_SOURCE_CHANGE_DATA_01)
+        rows = self.db.query(QUERY_SELECT_EVENTS_BY_EVENT_DATE)
+        print(rows.export('csv'))
+
+
+def load_status_history_data(db, status_history_data):
+    status_history_file_object = StringIO.StringIO(status_history_data)
+    reader = csv.DictReader(status_history_file_object)
+    for row in reader:
+        entity_id = row['instance_id']
+        timestamp = datetime_to_timestamp(datetime.datetime.now(tz=utc_timezone))
+        event_date = datetime_to_timestamp(parse(row['start_date']))
+        event_topic = 'atmosphere.model.instance#Instance.StatusChanged'
+        event_attrs = row
+        event_attrs_json = json.dumps(event_attrs)
+        hash = md5(event_attrs_json).digest()
+        event_id = str(uuid.UUID(bytes=hash[:16], version=3))
+        db.query(QUERY_INSERT_EVENT,
+                 entity_id=entity_id,
+                 event_id=event_id,
+                 timestamp=timestamp,
+                 event_date=event_date,
+                 event_topic=event_topic,
+                 event_attrs=event_attrs_json)
+
+
+def load_allocation_source_change_data(db, allocation_source_change_data):
+    allocation_source_change_data_file_object = StringIO.StringIO(allocation_source_change_data)
+    reader = csv.DictReader(allocation_source_change_data_file_object)
+    for row in reader:
+        timestamp = datetime_to_timestamp(datetime.datetime.now(tz=utc_timezone))
+        event_date = datetime_to_timestamp(parse(row['event_date']))
+        event_topic = 'atmosphere.model.instance#Instance.AllocationSourceChanged'
+        event_attrs = row
+        event_attrs_json = json.dumps(event_attrs)
+        entity_id = row['instance_id']
+        hash = md5(event_attrs_json).digest()
+        event_id = str(uuid.UUID(bytes=hash[:16], version=3))
+
+        db.query(QUERY_INSERT_EVENT,
+                 entity_id=entity_id,
+                 event_id=event_id,
+                 timestamp=timestamp,
+                 event_date=event_date,
+                 event_topic=event_topic,
+                 event_attrs=event_attrs_json)
 
 
 if __name__ == '__main__':
